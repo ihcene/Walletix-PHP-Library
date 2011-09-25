846e84febd5c7cd5b3b5231f7c623d547821c9ab
djug theDjug <djugprog@gmail.com>
2011-09-25 14:16:06 +0100

    Walletix PHP Library

--- exemple.php
+++ exemple.php
@@ -1,14 +1,12 @@
 <?php
-
 ERROR_REPORTING(E_ALL);
-
 require 'walletix.php';
-
 
 // ==============================
 // = Soit via le constructeur   =
 // ==============================
 $api = new WalletixAPI('YOUR_vendorID_HERE', 'YOUR_apiKey_HERE');
+
 
 // =========================
 // = Soit avec les setters =
@@ -18,49 +16,38 @@ $api->setApiKey('YOUR_apiKey_HERE');
 
 
 // =================================
-// = gÈnÈrer un code de paiement   =
-// = commande N∞1 , montant 500 DA =
+// = g√©n√©rer un code de paiement   =
+// = commande N¬∞1 , montant 500 DA =
 // =================================
-
 $response = $api->generatePaiementCode(1, 500);
 
-if (WALLETIX_GENCODE_OK== $response->status) {
+if ($response->status == WALLETIX_GENCODE_OK) {
   echo "Votre code de paiement est {$response->code} <br/>";
- } else{
-	
-	echo "erreur lors de la gÈnÈration du code de paiement<br />";
+}else{
+  echo "erreur lors de la g√©n√©ration du code de paiement<br />";
+} 
 
- } 
- 
+
 // ================================================================
-// = vÈrifier un code de confirmation   						  =
+// = v√©rifier un code de confirmation                             =
 // = code de paiement  73DEB7FAF0 code de confirmation D726A1BEEA =
 // ================================================================
 
-	$paiementCode="73DEB7FAF0";
-	$confirmationCode="D726A1BEEA";
+$paiementCode="73DEB7FAF0";
+$confirmationCode="D726A1BEEA";
 
-	$response = $api->verifyCode($paiementCode, $confirmationCode);
-	
-	if (WALLETIX_VERCODE_OK == $response->status){
-		
-		if (WALLETIX_CONFCODE_OK== $response->result) {
-	  
-	
-	  	echo"<br/>Votre paiement a Ètait bien effectuÈ <BR /> 
-			votre commande vous sera envoyÈe prochainement.";	
-	  }else{
-			echo "Code de confirmation erronÈe";
+$response = $api->verifyCode($paiementCode, $confirmationCode);
 
-		}
-	
-	
-	}else{
-			echo "erreur lors de test du code de confirmation<br />";
-	
-	}
-	
-	  
-	  
+if ($response->status == WALLETIX_VERCODE_OK){
+  if ($response->result == WALLETIX_CONFCODE_OK) {
+    echo"<br/>Votre paiement a √©tait bien effectu√© <br /> votre commande vous sera envoy√©e prochainement.";
+  }
+  else{
+    echo "Code de confirmation erron√©e";
+  }
+}
+else{
+    echo "erreur lors de test du code de confirmation<br />";
+}
 
 ?>
